01b19ff67fa687c89a38f3b5dc7af8fb1d03d7ec
Brunoxd23 <monteiro_100@outlook.pt>
2024-02-29 21:27:50 -0300

    chore: add initial prisma shema

--- app/(home)/booking-item.tsx
+++ app/(home)/booking-item.tsx
@@ -1,57 +1,49 @@
-import { useEffect, useState } from 'react';
-import { PrismaClient } from '@prisma/client';
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../_components/avatar";
 
-const prisma = new PrismaClient();
+import { Input } from "../_components/input";
+import { Label } from "../_components/label";
 
-function BookingItem() {
-  const [nome, setNome] = useState('');
-  const [aluno, setAluno] = useState(null);
 
-  useEffect(() => {
-    // Verifica se estamos no navegador antes de usar useState
-    if (typeof window !== 'undefined') {
-      const handleSearch = async () => {
-        try {
-          const alunoEncontrado = await prisma.estudante.findUnique({
-            where: {
-              nome,
-            },
-          });
-          setAluno(alunoEncontrado);
-        } catch (error) {
-          console.error('Erro ao buscar aluno:', error);
-        }
-      };
+function Booking() {
+    return (
 
-      // Definindo handleSearch como uma função global disponível no navegador
-      window.handleSearch = handleSearch;
-    }
-  }, [nome]);
+<div className="flex justify-center items-center h-screen">
+    <Card className="p-2 md:p-4 max-w-md md:max-w-lg">
+        <CardContent className="pb-2">
+            <h1 className="text-lg md:text-2xl mb-6 text-right">Carômetro Aluno</h1>
 
-  return (
-    <div className="flex justify-center items-center h-screen">
-      {/* Seção de pesquisa */}
-      <div>
-        <input
-          type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          placeholder="Digite o nome do aluno"
-        />
-        <button onClick={window.handleSearch}>Pesquisar</button>
-      </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+                <div>
+                    <div className="mb-4">
+                        <Label htmlFor="matricula">Matrícula</Label>
+                        <Input type="text" id="matricula" placeholder="Matrícula" />
+                    </div>
+                    <div className="mb-4">
+                        <Label htmlFor="name">Nome</Label>
+                        <Input type="text" id="name" placeholder="Nome" />
+                    </div>
+                    <div className="mb-4" >
+                        <Label htmlFor="curso">Curso</Label>
+                        <Input type="text" id="curso" placeholder="Curso" />
+                    </div>
+                </div>
+                <div className="flex justify-center">
+                    <Avatar className="h-24 w-24 md:h-40 md:w-40">
+                        <AvatarImage src="https://utfs.io/f/3bd228b5-8e39-44c2-8dbb-ec2064d972cb-12dp52.jpg" />
+                        <AvatarFallback>A</AvatarFallback>
+                    </Avatar>
+                </div>
+            </div>
+        </CardContent>
+    </Card>
+</div>
 
-      {/* Exibição dos dados do aluno */}
-      {aluno && (
-        <div>
-          <p>Matrícula: {aluno.ra}</p>
-          <p>Nome: {aluno.nome}</p>
-          <p>Curso: {aluno.curso}</p>
-          <img src={aluno.fotoUrl} alt="Foto do Aluno" />
-        </div>
-      )}
-    </div>
-  );
+
+
+    );
+
+    
 }
-
-export default BookingItem;
+ 
+export default Booking;
--- prisma/migrations/20240301002115_add_initial_tables/migration.sql
+++ prisma/migrations/20240301002115_add_initial_tables/migration.sql
@@ -1,10 +1,9 @@
 -- CreateTable
 CREATE TABLE "Estudante" (
-    "id" SERIAL NOT NULL,
     "ra" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
     "curso" TEXT NOT NULL,
-    "foto" TEXT NOT NULL,
+    "fotoUrl" TEXT NOT NULL,
 
-    CONSTRAINT "Estudante_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Estudante_pkey" PRIMARY KEY ("ra")
 );
